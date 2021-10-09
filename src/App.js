8e18b81232a5d0c19f051fee3aa2681668ac277b
ashiftpbm <85751695+ashiftpbm@users.noreply.github.com>
2021-10-09 19:36:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import AddProduct from './components/AddProduct';
+import ProductList from './components/ProductList';
+
+import Context from "./Context";
+import "./App.css";
+
+export default class App extends Component {
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    this.setState({ products }, () => callback && callback());
+  };
+
+
+  render() {
+    return (
+      <Context.Provider>
+        <Router ref={this.routerRef}>
+        <div className="App">
+          <nav
+            className="navbar container">
+            <div className="navbar-brand">
+              <img className ="logo" src= "https://media.istockphoto.com/vectors/vector-logo-letter-d-wing-vector-id1012390044?k=20&m=1012390044&s=612x612&w=0&h=r08VK9ey2lZXiSAdEn4u1bqm8bOix7vftY-Tu-noZDs="/>
+              <b className="navbar-head">Dailymart</b>
+            </div>
+            
+                <Link to="/products" className="navbar-item">
+                    Products
+                </Link>
+                <span className="navbar-item">
+                  Cart
+                </span>
+                <span className="navbar-item">
+                    Login
+                 </span>
+            </nav>
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/products" component={ProductList} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
+}
